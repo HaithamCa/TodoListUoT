eba1cd036a4336bd0881a04e6169e1ef7f71bfe2
hytham defrawy <hythamdefrawy@gmail.com>
2023-10-24 16:46:52 -0400

    First design

---
+++ TodoListUoT/File.swift
@@ -0,0 +1,16 @@
+//
+//  File.swift
+//  TodoListUoT
+//
+//  Created by hytham defrawy on 2023-10-22.
+//
+
+import Foundation
+
+let tasks = [
+    "Task 1",
+    "Task 2",
+    "Task 3",
+    "Task 4",
+    "Task 5"
+]
